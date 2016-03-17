902582f17c7c8a8469a775e85462805a390fa062
codeamac17 <codeamac17@gmail.com>
2016-03-17 15:11:13 -0600

    modificaciones log in

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+class CreateUserOrganizations < ActiveRecord::Migration
+  def change
+    create_table :user_organizations do |t|
+      t.string :category_location
+      t.string :profile
+      t.string :name
+      t.string :description
+      t.string :address
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
+
+
+ 
